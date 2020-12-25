4eb3af2f491f07e06cf80685bc000cab0f28af55
xinller <xl1012796466@gmail.com>
2020-12-25 14:52:26 +0800

    update

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,7 +2,7 @@
     <div class="home-page">
         <div class="banner">
             <div class="container">
-                <h1 class="logo-font">拉勾教育-rr</h1>
+                <h1 class="logo-font">拉勾教育-</h1>
                 <p>A place to share your knowledge.</p>
             </div>
         </div>
